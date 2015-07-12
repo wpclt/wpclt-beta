075f86a915a3238673541681d15429c25240b18f
Jamie Bowman <mrjamiebowman@gmail.com>
2015-07-12 19:22:02 -0400

    Fixed icons reference paths.

--- wp-content/themes/wpclt/header.php
+++ wp-content/themes/wpclt/header.php
@@ -13,29 +13,29 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/wpclt.ico" />
+	<?php //<link rel="shortcut icon" href="<? php echo get_template_directory_uri(); ?>/wpclt.ico" /> ?>
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 	<![endif]-->
-	<link rel="apple-touch-icon" sizes="57x57" href="icons/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="icons/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="icons/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="icons/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="icons/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="icons/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="icons/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="icons/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="icons/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="icons/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/icons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/icons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/icons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/icons/favicon-16x16.png">
 	<link rel="manifest" href="/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="icons/ms-icon-144x144.png">
+	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/icons/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
 	<?php wp_head(); ?>
 </head>
